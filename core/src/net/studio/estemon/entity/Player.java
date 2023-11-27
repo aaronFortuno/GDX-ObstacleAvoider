17c06fb9b173565254fa307208dc89118264a3a8
aaronFortuno <aaron.fortuno@protonmail.com>
2023-11-27 07:13:19 +0100

    remove imports

--- core/src/net/studio/estemon/entity/Player.java
+++ core/src/net/studio/estemon/entity/Player.java
@@ -1,10 +1,5 @@
 package net.studio.estemon.entity;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Circle;
-
 import net.studio.estemon.config.GameConfig;
 
 public class Player extends GameObjectBase {
